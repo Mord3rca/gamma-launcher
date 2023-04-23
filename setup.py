8be5368de289017e665c3b40eefd5d71dcb259b0
Mord3rca <morderca@morderca.net>
2023-04-23 19:34:08 +0200

    Adding setup file, flake8 config, gitignore & README.md

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# More info: https://github.com/navdeep-G/setup.py
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'launcher', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    requires=["requests", "rarfile", "py7zr"],
+    packages=['launcher', 'launcher.commands', 'launcher.downloader'],
+    entry_points={
+        'console_scripts': [
+            'gamma-launcher = launcher.cli:main',
+        ],
+    },
+    python_requires='>=3.11.0',
+    license=about['__license__'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.11'
+    ]
+)
