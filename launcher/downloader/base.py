54c49137e1c80d2cfa42e073ecd980834b31f4fb
Mord3rca <morderca@morderca.net>
2023-04-23 19:34:41 +0200

    Launcher: Adding `launcher.downloader`

---
+++ launcher/downloader/base.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from hashlib import md5
+
+from requests import Session
+
+g_session = Session()
+g_session.headers.update({'User-Agent': 'pyGammaLauncher'})
+
+
+class Base(ABC):
+
+    def __init__(self, url: str) -> None:
+        self._url = url
+        self._md5 = None
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def md5(self) -> str:
+        if not self._md5:
+            raise RuntimeError("md5 not calculated, please use download() first")
+
+        return self._md5
+
+    @property
+    @abstractmethod
+    def filename(self) -> str:
+        pass
+
+    def download(self, path: str) -> None:
+        h = md5()
+        with open(path, 'wb') as f:
+            r = g_session.get(self._url)
+            for chunk in r.iter_content(chunk_size=1 * 1024 * 1024):
+                if chunk:
+                    f.write(chunk)
+                    h.update(chunk)
+        self._md5 = h.hexdigest()
---
+++ launcher/downloader/moddb.py
@@ -0,0 +1,31 @@
+from .base import Base, g_session
+from urllib.parse import urlparse
+
+import re
+import os.path
+
+
+class ModDB(Base):
+
+    def __init__(self, url: str) -> None:
+        super().__init__(url)
+        self._orig_url = url
+        self._filename = None
+
+    @property
+    def url(self) -> str:
+        return self._orig_url
+
+    @property
+    def filename(self) -> str:
+        if not self._filename:
+            self._get_filename()
+
+        return self._filename
+
+    def _get_filename(self) -> None:
+        id = self._url.split('/')[-1]
+        s = re.search(f'/downloads/mirror/{id}/[^"]*', g_session.get(self._url).text)
+        location = g_session.get(f"https://www.moddb.com{s[0]}", allow_redirects=False).headers["location"]
+        self._url = location
+        self._filename = os.path.basename(urlparse(location).path)
