63c2ae19ffcaade4aec541587061a2dfc28d20da
Mord3rca <morderca@morderca.net>
2023-04-24 19:05:31 +0200

    cli: Adding subcommand support for futur devs

--- launcher/cli.py
+++ launcher/cli.py
@@ -3,12 +3,48 @@ from launcher.commands import FullInstall
 from argparse import ArgumentParser
 
 
+def command_object_to_dict(o):
+    return {
+        o.name: {
+            "help": o.help,
+            "arguments": o.arguments,
+            "cobject": o,
+        }
+    }
+
+
+parser_desc = {
+     "description": "Launcher for S.T.A.L.K.E.R.: G.A.M.M.A.",
+     # Global arguments
+     "arguments": {},
+     "subparsers": {
+         "help": "Command to invoke",
+         "dest": "command",
+         "list": {
+             **command_object_to_dict(FullInstall),
+         },
+     },
+}
+
+
 def main():
-    parser = ArgumentParser(
-        description="Setting up S.T.A.L.K.E.R. G.A.M.M.A",
+    global parser_desc
+
+    parser = ArgumentParser(description=parser_desc["description"])
+    for m, a in parser_desc['arguments'].items():
+        parser.add_argument(m, **a)
+
+    subparsers = parser_desc['subparsers']
+    sp = parser.add_subparsers(
+        help=subparsers["help"], dest=subparsers["dest"]
     )
-    parser.add_argument('--anomaly', type=str, default=None, help="Path to ANOMALY directory")
-    parser.add_argument('--gamma', type=str, default=None, help="Path to GAMMA directory")
+    sp.required = True
+
+    for n, p in subparsers['list'].items():
+        subparser = sp.add_parser(n, help=p['help'])
+        for m, a in p["arguments"].items():
+            subparser.add_argument(m, **a)
+        subparser.set_defaults(cobject=p['cobject'])
 
     args = parser.parse_args()
-    FullInstall().run(args)
+    args.cobject().run(args)
