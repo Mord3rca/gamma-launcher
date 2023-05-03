dff537109fb77546241c721d7ef05b41149e0978
Mord3rca <morderca@morderca.net>
2023-05-03 18:28:03 +0200

    `test-mod-maker`: Moving directory check in its own method

--- launcher/commands/tests.py
+++ launcher/commands/tests.py
@@ -1,6 +1,5 @@
-import os.path
-
 from pathlib import Path
+from typing import Dict
 
 from launcher.archive import list_archive_content
 from launcher.commands.common import read_mod_maker
@@ -21,28 +20,41 @@ class TestModMaker:
 
     help: str = "Testing mod maker directives"
 
+    def __init__(self):
+        self.modpack_dl_dir = None
+        self.modpack_data_dir = None
+
+    def _check_install_directives(self, mod: Dict):
+        if not mod['install_directives']:
+            return
+
+        e = get_handler_for_url(mod['url'])
+        archive = self.modpack_dl_dir / e.filename
+
+        if not archive.exists():
+            print(f"Downloading missing file: {archive.name}")
+            e.download(archive)
+
+        content = list_archive_content(archive)
+        if not content:
+            print(f"WARNING: {archive.name} is empty")
+            return
+
+        for d in mod['install_directives']:
+            for f in content:
+                if d in f:
+                    break
+            else:
+                print(f'Warning: "{d}" is not in "{archive.name}"')
+
     def run(self, args) -> None:
-        modpack_dl_dir = Path(args.gamma) / "downloads"
-        modpack_data_dir = Path(args.gamma) / ".Grok's Modpack Installer" / "G.A.M.M.A" / "modpack_data"
+        self.modpack_dl_dir = Path(args.gamma) / "downloads"
+        self.modpack_data_dir = Path(args.gamma) / ".Grok's Modpack Installer" / "G.A.M.M.A" / "modpack_data"
 
         mod_maker = read_mod_maker(
-            modpack_data_dir / 'modlist.txt',
-            modpack_data_dir / 'modpack_maker_list.txt'
+            self.modpack_data_dir / 'modlist.txt',
+            self.modpack_data_dir / 'modpack_maker_list.txt'
         )
 
-        for i in filter(lambda v: v and v['install_directives'], mod_maker.values()):
-            e = get_handler_for_url(i['url'])
-            archive = modpack_dl_dir / e.filename
-
-            if not archive.exists():
-                print(f"Downloading missing file: {archive.name}")
-                e.download(archive)
-
-            content = [i.strip(os.path.sep) for i in list_archive_content(archive)]
-            if not content:
-                print(f"WARNING: {archive.name} is empty")
-                continue
-
-            for d in i['install_directives']:
-                if d not in content:
-                    print(f'Warning: "{d}" is not in "{archive.name}"')
+        for mod in filter(lambda x: x, mod_maker.values()):
+            self._check_install_directives(mod)
