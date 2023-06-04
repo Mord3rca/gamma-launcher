6c12af0ca086962053937b047a22d53c7adacaac
Singustromo <68005562+Singustromo@users.noreply.github.com>
2023-06-04 08:03:14 +0000

    Streamline check-md5 output

--- launcher/commands/check.py
+++ launcher/commands/check.py
@@ -55,23 +55,23 @@ class CheckMD5:
 
             with open(file, 'rb') as f:
                 md5 = file_digest(f, 'md5').hexdigest()
-            print(f"{file.name} remote hash is: '{info['MD5 Hash']}'")
-            print(f"{file.name} local  hash is: '{md5}'")
 
-            if md5 != info['MD5 Hash']:
-                if redownload:
-                    print(f"Redownloading {file.name}...")
-                    if self.redownload(i, args.gamma):
-                        print(f"{file.name} downloaded successfully")
-                    else:
-                        error = f"Error: {file.name} failed MD5 check after being redownloaded"
-                        print(error)
-                        errors += [error]
-                else:
-                    errors += [f"Error: {file.name} -- remote({info['MD5 Hash']}) != local({md5})"]
-                    print('  !! Please update your installation')
+            print(f"{file.name} ({info['MD5 Hash']}): ", end='')
+            print("OK" if info["MD5 Hash"] == md5 else "MISMATCH")
 
-            print('-' * 25)
+            if info["MD5 Hash"] == md5: continue
+            if not redownload:
+                errors += [f"Error: {file.name} -- remote({info['MD5 Hash']}) != local({md5})"]
+                print('  !! Please update your installation')
+                continue
+
+            print(f"Redownloading {file.name}...")
+            if self.redownload(i, args.gamma):
+                print(f"{file.name} downloaded successfully")
+            else:
+                error = f"Error: {file.name} failed MD5 check after being redownloaded"
+                print(error)
+                errors += [error]
 
         for err in errors:
             print(err)
@@ -97,7 +97,7 @@ class CheckMD5:
         info = parse_moddb_data(dict["info_url"])
         with open(file, 'rb') as f:
             local_md5 = file_digest(f, 'md5').hexdigest()
-        print(f"{info['Filename']} remote hash is: '{info['MD5 Hash']}'")
-        print(f"{info['Filename']} local  hash is: '{local_md5}'")
 
+        print(f"{info['Filename']} ({info['MD5 Hash']}): ", end='')
+        print("OK" if info["MD5 Hash"] == local_md5 else "MISMATCH")
         return info["MD5 Hash"] == local_md5
