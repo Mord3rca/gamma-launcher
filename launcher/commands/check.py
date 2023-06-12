300ebb7c20ad36ee4759e04e64d78dda74b3fc79
Mord3rca <morderca@morderca.net>
2023-06-12 17:49:25 +0200

    More error handling

--- launcher/commands/check.py
+++ launcher/commands/check.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from tenacity import RetryError
 
 from launcher.commands.common import read_mod_maker, parse_moddb_data
 from launcher.downloader import download_mod
@@ -57,7 +58,12 @@ class CheckMD5:
             self.register_err(f"{file.name} not found on disk", show=False)
             return
 
-        file = download_mod(url, self._dl_dir, use_cached=False)
+        try:
+            file = download_mod(url, self._dl_dir, use_cached=False)
+        except RetryError:
+            self._register_err(f"Failed to download {file.name}")
+            return
+
         if not self._test_hash(file, hash):
             self.register_err(f"Failed to download missing file - {file.name}")
 
@@ -100,7 +106,12 @@ class CheckMD5:
                 self.register_err(f"{file.name} MD5 missmatch")
                 continue
 
-            file = download_mod(i['url'], self._dl_dir, use_cached=False)
+            try:
+                file = download_mod(i['url'], self._dl_dir, use_cached=False)
+            except RetryError:
+                self._register_err(f"Failed to redownload {file.name}")
+                continue
+
             if not self._test_hash(file, info['MD5 Hash']):
                 self.register_err(f"{file.name} failed MD5 check after being redownloaded")
 
