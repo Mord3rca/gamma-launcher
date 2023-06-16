107e2c191a2fe0a3c32c496be530d45742680072
Mord3rca <morderca@morderca.net>
2023-06-16 15:47:22 +0200

    `check-md5`: Reducing complexity of `run()` method to fix C901 defect

--- launcher/commands/check.py
+++ launcher/commands/check.py
@@ -31,16 +31,41 @@ class CheckMD5:
     help: str = "Check MD5 hash for all addons"
 
     def __init__(self) -> None:
+        self._gamma = None
+        self._dl_dir = None
         self._errors = []
+        self._update_cache = False
 
     def register_err(self, err: str, show: bool = True, level: str = "Error") -> None:
         self._errors += [f"{level}: {err}"]
         if show:
             print(f"  !! {err}")
 
+    def _test_hash(self, file: Path, hash: str) -> bool:
+        with open(file, 'rb') as f:
+            md5 = file_digest(f, 'md5').hexdigest()
+
+        valid = md5 == hash
+
+        print(f"{file.name} ({hash}): ", end='')
+        print("OK" if valid else "MISMATCH")
+
+        return valid
+
+    def _if_file_missing(self, file: Path, url: str, hash: str) -> None:
+        if not self._update_cache:
+            self.register_err(f"{file.name} not found on disk", show=False)
+            return
+
+        file = download_mod(url, self._dl_dir, use_cached=False)
+        if not self._test_hash(file, hash):
+            self.register_err(f"Failed to download missing file - {file.name}")
+
     def run(self, args) -> None:
-        modpack_dl_dir = Path(args.gamma) / "downloads"
-        modpack_data_dir = Path(args.gamma) / ".Grok's Modpack Installer" / "G.A.M.M.A" / "modpack_data"
+        self._gamma = Path(args.gamma)
+        self._update_cache = args.update_cache
+        self._dl_dir = self._gamma / "downloads"
+        modpack_data_dir = self._gamma / ".Grok's Modpack Installer" / "G.A.M.M.A" / "modpack_data"
 
         mod_maker = read_mod_maker(
             modpack_data_dir / 'modlist.txt',
@@ -51,7 +76,8 @@ class CheckMD5:
         for i in filter(lambda v: v and v['info_url'], mod_maker.values()):
             try:
                 info = parse_moddb_data(i['info_url'])
-                file = modpack_dl_dir / info['Filename']
+                file = self._dl_dir / info['Filename']
+                hash = info['MD5 Hash']
             except KeyError:
                 self.register_err(f"Can't parse moddb page for {i['info_url']}")
                 continue
@@ -64,56 +90,19 @@ class CheckMD5:
                 continue
 
             if not file.exists():
-                if args.update_cache:
-                    self.redownload(i, args.gamma)
-                else:
-                    self.register_err(f"{file.name} not found on disk", show=False)
+                self._if_file_missing(file, i['url'], hash)
                 continue
 
-            with open(file, 'rb') as f:
-                md5 = file_digest(f, 'md5').hexdigest()
-
-            print(f"{file.name} ({info['MD5 Hash']}): ", end='')
-            print("OK" if info["MD5 Hash"] == md5 else "MISMATCH")
-
-            if info["MD5 Hash"] == md5:
+            if self._test_hash(file, info['MD5 Hash']):
                 continue
 
             if not args.update_cache:
-                self.register_err(f"{file.name} -- remote({info['MD5 Hash']}) != local({md5})")
+                self.register_err(f"{file.name} MD5 missmatch")
                 continue
 
-            print(f"Redownloading {file.name}...")
-            if self.redownload(i, args.gamma):
-                print(f"{file.name} downloaded successfully")
-            else:
+            file = download_mod(i['url'], self._dl_dir, use_cached=False)
+            if not self._test_hash(file, info['MD5 Hash']):
                 self.register_err(f"{file.name} failed MD5 check after being redownloaded")
 
         for err in self._errors:
             print(err)
-
-    def redownload(self, dict: dict, gamma_dir: str) -> bool:
-        """
-        Downloads and installs a specific mod, then checks its MD5 hash again.
-
-        :param dict:
-        Values gathered from the modpack_maker_list.txt file.
-        :param gamma_dir:
-        Path to the GAMMA folder.
-
-        :return:
-        Whether the local and remote MD5 hashes match.
-        """
-
-        # Download and install the mod:
-        download_dir = Path(gamma_dir).joinpath("downloads")
-        file = download_mod(dict["url"], download_dir, use_cached=False)
-
-        # Check MD5 sum again:
-        info = parse_moddb_data(dict["info_url"])
-        with open(file, 'rb') as f:
-            local_md5 = file_digest(f, 'md5').hexdigest()
-
-        print(f"{info['Filename']} ({info['MD5 Hash']}): ", end='')
-        print("OK" if info["MD5 Hash"] == local_md5 else "MISMATCH")
-        return info["MD5 Hash"] == local_md5
