ff6811804342b7ab194feab6e7d7a9ea6448d280
Mord3rca <morderca@morderca.net>
2023-12-04 21:43:34 +0100

    Permit to use direct URL to a repo for github downloader

--- launcher/downloader/github.py
+++ launcher/downloader/github.py
@@ -1,10 +1,28 @@
-from .base import Base
+from re import compile
+
+from .base import Base, g_session
 
 
 class Github(Base):
 
+    regexp_url = compile("https?://github.com/(\w+)/(\w+)/?")
+
+    def __init__(self, url: str) -> None:
+        super().__init__(url)
+        self._project = None
+
+        self._decode_url(url)
+
+    def _decode_url(self, url: str) -> None:
+        user, self._project = self.regexp_url.match(url).groups()
+
+        if(not url.endswith(".zip")):
+            branch = g_session.get(
+                f"https://api.github.com/repos/{user}/{self._project}",
+                headers={"Accept": "application/json"}
+            ).json()["default_branch"]
+            self._url = f"https://github.com/{user}/{self._project}/archive/refs/heads/{branch}.zip"
+
     @property
     def filename(self) -> str:
-        e = list(filter(None, self._url.split('/')))
-        filename = e[e.index('github.com') + 2]
-        return f"{filename}.zip"
+        return f"{self._project}.zip"
