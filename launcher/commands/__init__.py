8b484174216e309416fa24020192fdb68d915f0f
Mord3rca <morderca@morderca.net>
2023-12-10 17:18:27 +0100

    Adding check-anomaly command

--- launcher/commands/__init__.py
+++ launcher/commands/__init__.py
@@ -1,4 +1,4 @@
-from .check import CheckMD5
+from .check import CheckAnomaly, CheckMD5
 from .install import AnomalyInstall, FullInstall
 from .shader import RemoveReshade, PurgeShaderCache
 from .tests import TestModMaker
