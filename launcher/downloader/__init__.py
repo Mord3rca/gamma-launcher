61091e63da512c6e0c5bb83aa01068df2d167bd5
Mord3rca <morderca@morderca.net>
2023-12-10 15:59:02 +0100

    Renaming download_mod to download_archive and adding a check-hash feature

--- launcher/downloader/__init__.py
+++ launcher/downloader/__init__.py
@@ -7,6 +7,8 @@ from requests.exceptions import ConnectionError
 from tenacity import retry, stop_after_attempt, wait_fixed
 from pathlib import Path
 
+from launcher.compat import file_digest
+
 
 def get_handler_for_url(url: str) -> Base:
     host = urlparse(url).hostname
@@ -21,13 +23,27 @@ def get_handler_for_url(url: str) -> Base:
     stop=stop_after_attempt(3),
     wait=wait_fixed(5),
 )
-def download_mod(url: str, download_dir: Path, use_cached: bool = True) -> Path:
+def download_archive(
+    url: str, download_dir: Path, use_cached: bool = True,
+    hash_type: str = 'md5', hash: str = None
+) -> Path:
     e = get_handler_for_url(url)
     file = Path(download_dir, e.filename)
 
     if file.is_file() and use_cached:
-        print(f'  - Using cached {file.name}')
-        return file
+        if not hash:
+            print(f'  - Using cached {file.name}')
+            return file
+
+        with open(file, 'rb') as f:
+            fhash = file_digest(f, hash_type).hexdigest()
+
+        if (fhash == hash):
+            print(f'  - Using cached {file.name} ({hash})')
+            return file
+
+        print(f'  - Updating cache file {file.name}')
+        file.unlink()  # Redownload
 
     print(f'  - Downloading {file.name}')
     try:
@@ -38,3 +54,7 @@ def download_mod(url: str, download_dir: Path, use_cached: bool = True) -> Path:
         raise e
 
     return file
+
+
+def download_mod(*args, **kwargs) -> Path:
+    return download_archive(*args, **kwargs)
