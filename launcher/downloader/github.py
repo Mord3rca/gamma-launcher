ac7bf42c42c8909eb214942b16a197581ecfdf67
Mord3rca <morderca@morderca.net>
2023-12-23 12:11:00 +0100

    launcher.downloader.github: Do not overwrite URL if a release is used

--- launcher/downloader/github.py
+++ launcher/downloader/github.py
@@ -9,20 +9,22 @@ class Github(Base):
 
     def __init__(self, url: str) -> None:
         super().__init__(url)
-        self._project = None
+        self._filename = super().filename
 
-        self._decode_url(url)
+        self._decode_url()
 
-    def _decode_url(self, url: str) -> None:
-        user, self._project = self.regexp_url.match(url).groups()
+    def _decode_url(self) -> None:
+        if "release" in self._url or self._url.endswith(".zip"):
+            return
 
-        if (not url.endswith(".zip")):
-            branch = g_session.get(
-                f"https://api.github.com/repos/{user}/{self._project}",
-                headers={"Accept": "application/json"}
-            ).json()["default_branch"]
-            self._url = f"https://github.com/{user}/{self._project}/archive/refs/heads/{branch}.zip"
+        user, project = self.regexp_url.match(self._url).groups()
+        branch = g_session.get(
+            f"https://api.github.com/repos/{user}/{project}",
+            headers={"Accept": "application/json"}
+        ).json()["default_branch"]
+        self._url = f"https://github.com/{user}/{project}/archive/refs/heads/{branch}.zip"
+        self._filename = f"{project}.zip"
 
     @property
     def filename(self) -> str:
-        return f"{self._project}.zip"
+        return self._filename
