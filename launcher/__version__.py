f33403f1f57c16cce9c5a1c6d4dd91eca39b6157
Mord3rca <morderca@morderca.net>
2024-03-20 17:36:32 +0100

    Setting version to 1.6

--- launcher/__version__.py
+++ launcher/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "gamma-launcher"
 __description__ = "G.A.M.M.A. Launcher module"
 __url__ = "https://github.com/Mord3rca/gamma-launcher"
-__version__ = "1.5"
+__version__ = "1.6"
 __author__ = "Mord3rca"
 __author_email__ = "morderca@morderca.net"
 __license__ = "GPLv3"
