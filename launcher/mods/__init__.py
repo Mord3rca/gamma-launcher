9c12138f93ad249e11543d23e18083710b60e267
Mord3rca <morderca@morderca.net>
2024-09-19 21:44:14 +0200

    Moving installation logic to `launcher.mods.installer`

--- launcher/mods/__init__.py
+++ launcher/mods/__init__.py
@@ -1,44 +1,72 @@
 from os.path import sep
 from pathlib import Path
-from typing import Dict, List
+from typing import List
 
-from launcher.mods.base import Base, Default
-from launcher.mods.git import GitInstaller
-from launcher.mods.separator import Separator
-
+from launcher.archive import extract_archive
+from launcher.mods.base import ModBase
+from launcher.mods.installer import GitInstaller, SeparatorInstaller, DefaultInstaller
+from launcher.mods.downloader import ModDBDownloader, GithubDownloader, DefaultDownloader
 
 __all__ = [
     'read_mod_maker'
 ]
 
 
-def _read_mod_maker_line(line: str) -> Dict[str, str]:
-    it = line.split('\t')
-
-    return {
-        'name': f'{it[3]}{it[2]}',
-        'url': it[0],
-        'install_directives': [
-            i.replace('\\', sep).lstrip(sep) for i in it[1].split(':')
-        ] if it[1] != '0' else None,
-        'author': it[2].strip('- '),
-        'title': it[3].strip(),
-        'info_url': it[4] if len(it) >= 5 else '',
-    }
+class ModDefault(DefaultInstaller, DefaultDownloader, ModBase):
+    pass
 
 
-def register_git_mod(git_mods, **kwargs):
-    tmp = list(filter(lambda x: x.url == kwargs.get('info_url'), git_mods))
+class BaseArchive(DefaultInstaller, DefaultDownloader, ModBase):
+
+    def __init__(self, url: str) -> None:
+        super().__init__(None, url, None, None, None, None)
+
+
+class ModDBArchive(ModDBDownloader, ModDefault):
+
+    def __init__(self, name: str, url: str, iurl: str) -> None:
+        super().__init__(name, url, None, None, iurl, None)
+
+    def check(self, *args, **kwargs) -> None:
+        raise NotImplementedError("Not available")
+
+    def install(self, to: Path) -> None:
+        if not self._archive:
+            raise RuntimeError("Use download() method first")
+
+        extract_archive(self._archive, to, "application/x-7z-compressed+bcj2")
+
+
+class GithubArchive(GithubDownloader, ModDefault):
+
+    def __init__(self, url: str) -> None:
+        super().__init__(None, url, None, None, None, None)
+
+
+class ModDBInstaller(ModDBDownloader, DefaultInstaller):
+    pass
+
+
+class ModSeparator(SeparatorInstaller, ModBase):
+    pass
+
+
+class ModGitInstaller(GithubDownloader, GitInstaller, ModDefault):
+    pass
+
+
+def _register_git_mod(git_mods: List[ModGitInstaller], **kwargs):
+    tmp = list(filter(lambda x: x.url == kwargs.get('iurl'), git_mods))
     if tmp:
         tmp[0].append(**kwargs)
         return
 
-    obj = GitInstaller(kwargs.get('info_url'))
+    obj = ModGitInstaller(kwargs.get('iurl'))
     obj.append(**kwargs)
     git_mods += [obj]
 
 
-def read_mod_maker(mod_path: Path) -> List[Base]:
+def read_mod_maker(mod_path: Path) -> List[ModBase]:
     result = []
     git_mods = []
 
@@ -50,15 +78,25 @@ def read_mod_maker(mod_path: Path) -> List[Base]:
         )
     }
 
-    for i in filter(
+    for line in filter(
         lambda x: x and not x.startswith(' '),
         (mod_path / 'modpack_maker_list.txt').read_text().split('\n')
     ):
         try:
-            data = _read_mod_maker_line(i)
+            it = line.split('\t')
+            data = {
+                'name': f'{it[3]}{it[2]}',
+                'url': it[0],
+                'add_dirs': [
+                    i.replace('\\', sep).lstrip(sep) for i in it[1].split(':')
+                ] if it[1] != '0' else None,
+                'author': it[2].strip('- '),
+                'title': it[3].strip(),
+                'iurl': it[4] if len(it) >= 5 else '',
+            }
             name = data['name']
         except ValueError:
-            print(f'   Skipping: {i}')
+            print(f'   Skipping: {line}')
 
         for i in modlist.keys():
             if data['title'] in i:
@@ -67,36 +105,40 @@ def read_mod_maker(mod_path: Path) -> List[Base]:
 
     for name, data in modlist.items():
         if 'separator' in name:
-            result.append(Separator(name=name))
+            result.append(ModSeparator(name))
             continue
 
         if not data:
             continue
 
-        if 'addons/start/222467' in data.get('url') and 'github.com' in data.get('info_url'):
-            register_git_mod(git_mods, **data)
+        if 'addons/start/222467' in data.get('url') and 'github.com' in data.get('iurl'):
+            _register_git_mod(git_mods, **data)
             continue
 
-        result.append(Default(**data))
+        if 'moddb.com' in data.get('url'):
+            result.append(ModDBInstaller(**data))
+            continue
+
+        result.append(ModDefault(**data))
 
     # Not in the list but installed by Official Launcher. Not gonna ask why.
-    register_git_mod(git_mods, **{
+    _register_git_mod(git_mods, **{
         'name': 'Burn\'s Optimised World Models',
         'url': 'https://www.moddb.com/addons/start/222467',
-        'install_directives': None,
+        'add_dirs': None,
         'author': 'Burn',
         'title': 'Burn\'s Optimised World Models',
-        'info_url': 'https://github.com/Grokitach/gamma_large_files_v2',
+        'iurl': 'https://github.com/Grokitach/gamma_large_files_v2',
     })
 
-    # In the list but don't have info_url so this is ignored.
-    register_git_mod(git_mods, **{
+    # In the list but don't have iurl so this is ignored.
+    _register_git_mod(git_mods, **{
         'name': 'Retrogue\'s Additional Weapons',
         'url': 'https://www.moddb.com/addons/start/222467',
-        'install_directives': None,
+        'add_dirs': None,
         'author': 'Retrogue',
         'title': 'Retrogue\'s Additional Weapons',
-        'info_url': 'https://github.com/Grokitach/gamma_large_files_v2',
+        'iurl': 'https://github.com/Grokitach/gamma_large_files_v2',
     })
 
     return git_mods + result
---
+++ launcher/mods/installer/__init__.py
@@ -0,0 +1,7 @@
+from .default import DefaultInstaller
+from .git import GitInstaller
+from .separator import SeparatorInstaller
+
+__all__ = [
+    'DefaultInstaller', 'GitInstaller', 'SeparatorInstaller'
+]
