d39fac44e8dc41c5c5050c7156b002bb1e59acfb
Red007Master <56399742+Red007Master@users.noreply.github.com>
2025-06-19 04:34:40 +0300

    Bumping required avilible space for GAMMA setup.

--- launcher/commands/install.py
+++ launcher/commands/install.py
@@ -110,7 +110,7 @@ class GammaSetup:
 
     def run(self, args) -> None:
         if not args.cache_path:
-            check_tmp_free_space(5)
+            check_tmp_free_space(12)
 
         self._gamma_dir = Path(args.gamma).expanduser()
         self._grok_mod_dir = self._gamma_dir / ".Grok's Modpack Installer" / "G.A.M.M.A"
