feeec5e1a8bc8b8afbccb301b247e6b3ed273ef7
Mord3rca <morderca@morderca.net>
2026-02-18 15:20:37 +0100

    tests: Adding test cases for `launcher.mod.info` module

---
+++ tests/test_mods_info.py
@@ -0,0 +1,75 @@
+from unittest import TestCase
+
+from launcher.mods.info import ModInfo
+
+
+class ModInfoTestCase(TestCase):
+
+    _url: str = 'https://somewhere/on/the/internet'
+
+    _iurl: str = 'https://also/on/the/internet'
+
+    _full_dataset: dict = {
+        'author': 'mario', 'name': 'modname',
+        'title': '999- modname',
+        'url': _url, 'iurl': _iurl,
+        'subdirs': ['foobar']
+    }
+
+    _partial_data1: dict = {
+        'name': 'separator'
+    }
+
+    _partial_data2: dict = {
+        'url': _url,
+        'iurl': _iurl,
+        'subdirs': ['foo', 'bar']
+    }
+
+    def test_full_dataset(self):
+        m = ModInfo(self._full_dataset)
+
+        self.assertEqual(m.author, 'mario')
+        self.assertEqual(m.name, 'modname')
+        self.assertEqual(m.title, '999- modname')
+        self.assertEqual(m.url, self._url)
+        self.assertEqual(m.iurl, self._iurl)
+        self.assertEqual(
+            len(m.subdirs), len(self._full_dataset['subdirs']),
+            'Invalid number of subdirs'
+        )
+        self.assertEqual(m.subdirs[0], 'foobar', 'Invalid subdir')
+
+    def test_partial_data1(self):
+        m = ModInfo(self._partial_data1)
+
+        self.assertEqual(m.name, 'separator')
+        self.assertEqual(m.author, None)
+        self.assertEqual(m.title, None)
+        self.assertEqual(m.url, None)
+        self.assertEqual(m.iurl, None)
+        self.assertEqual(m.subdirs, None)
+
+    def test_partial_data2(self):
+        m = ModInfo(self._partial_data2)
+
+        self.assertEqual(m.name, None)
+        self.assertEqual(m.author, None)
+        self.assertEqual(m.title, None)
+        self.assertEqual(m.url, self._url)
+        self.assertEqual(m.iurl, self._iurl)
+        self.assertEqual(
+            len(m.subdirs), len(self._partial_data2['subdirs']),
+            'Invalid number of subdirs'
+        )
+        self.assertIn('foo', self._partial_data2['subdirs'])
+        self.assertIn('bar', self._partial_data2['subdirs'])
+
+    def test_set_name(self):
+        m = ModInfo(self._partial_data1)
+        new_name = '1337'
+
+        self.assertEqual(m.name, 'separator', 'Invalid name before set')
+
+        m.name = new_name
+        self.assertEqual(m.name, new_name, 'Invalid name after set')
