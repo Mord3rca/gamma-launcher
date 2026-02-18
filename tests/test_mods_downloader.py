51da856eb2ca3d54e522aecb02355053c679ae65
Mord3rca <morderca@morderca.net>
2026-02-18 15:33:14 +0100

    tests: Adding test cases for `launcher.mod.downloader` testing DownloaderFactory function

---
+++ tests/test_mods_downloader.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from launcher.mods.info import ModInfo
+from launcher.mods.downloader import DefaultDownloader, DownloaderFactory, GithubDownloader, ModDBDownloader
+
+
+class DownloaderFactoryTestCase(TestCase):
+
+    _info_moddb = ModInfo({'url': 'https://moddb.com/addons/start/1337'})
+
+    _info_git1 = ModInfo({'url': 'https://github.com/Mord3rca/gamma-launcher'})
+    _info_git2 = ModInfo({'url': 'https://github.com/Mord3rca/gamma-launcher/archive/refs/heads/master.zip'})
+
+    _random_archive = ModInfo({'url': 'https://somewhere/on/the/internet.rar'})
+
+    _info_none = ModInfo({})
+
+    def test_moddb_url(self):
+        o = DownloaderFactory(self._info_moddb)
+        self.assertIsInstance(o, ModDBDownloader)
+
+    def test_git_url(self):
+        o = DownloaderFactory(self._info_git1)
+        self.assertIsInstance(o, GithubDownloader)
+
+        o = DownloaderFactory(self._info_git2)
+        self.assertIsInstance(o, DefaultDownloader)
+
+    def test_random_archive(self):
+        o = DownloaderFactory(self._random_archive)
+        self.assertIsInstance(o, DefaultDownloader)
+
+    def test_none_url(self):
+        o = DownloaderFactory(self._info_none)
+        self.assertIsNone(o)
