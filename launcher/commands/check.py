b6f68ccf227136dc7586432c3e9ed527bd05c5f9
Mord3rca <morderca@morderca.net>
2026-02-20 20:02:27 +0100

    launcher.commands.check: Adding **--remove-unused** option to cleanup downloader directory of previous mod archives
    Resolve #242

--- launcher/commands/check.py
+++ launcher/commands/check.py
@@ -52,6 +52,11 @@ class CheckMD5:
             "required": False,
             "action": "store_true"
         },
+        "--remove-unused": {
+            "help": "After hash checks, remove unused archive in download directory",
+            "required": False,
+            "action": "store_true"
+        },
         "--redownload": {
             "help": SUPPRESS,
             "required": False,
@@ -63,6 +68,25 @@ class CheckMD5:
     name: str = "check-md5"
 
     help: str = "Check MD5 hash for all addons"
+
+    def _purge_unused_files(self, dl_dir: Path, downloaders) -> None:
+        files_in_use = set()
+        for i in downloaders:
+            try:
+                files_in_use.add(i.archive.absolute())
+            except Exception:
+                continue
+
+        # Extra files to keep from Anomaly Install / Gamma Setup commands
+        files_in_use.add(dl_dir / "Anomaly-1.5.3-Full.2.7z")
+        files_in_use.add(dl_dir / "modorganizer-Mod.Organizer-2.5.2.7z")
+
+        for archive in dl_dir.iterdir():
+            if archive.name.endswith(".git") or archive.absolute() in files_in_use:
+                continue
+
+            print(f"[+] Purging {archive}...")
+            archive.unlink()
 
     def run(self, args) -> None:
         errors = []
@@ -83,6 +107,9 @@ class CheckMD5:
                 i.check(dl_dir, args.update_cache)
             except (HashError, ModDBDownloadError) as e:
                 errors.append(str(e))
+
+        if args.remove_unused:
+            self._purge_unused_files(dl_dir, downloaders)
 
         print("\n".join(errors))
 
