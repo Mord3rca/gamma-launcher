135bf6e48dba46e25ab0465ad0e0133aad1fb9c5
Mord3rca <morderca@morderca.net>
2026-02-23 14:51:11 +0100

    Tiny reworks of checks for base & ModDB downloaders

--- launcher/mods/downloader/moddb.py
+++ launcher/mods/downloader/moddb.py
@@ -1,13 +1,10 @@
 import re
-import os.path
 
 from bs4 import BeautifulSoup
-from requests.exceptions import HTTPError
-from urllib.parse import urlparse
 from pathlib import Path
+from requests.exceptions import HTTPError
 from typing import Dict
 
-from launcher.hash import check_hash
 from launcher.exceptions import HashError, ModDBDownloadError
 from launcher.mods.downloader.base import DefaultDownloader, g_session
 
@@ -19,7 +16,7 @@ class ModDBDownloader(DefaultDownloader):
         self._iurl = iurl
 
     @staticmethod
-    def _parse_moddb_data(url: str) -> Dict[str, str]:
+    def _parse_moddb_metadata(url: str) -> Dict[str, str]:
         r = g_session.get(url)
 
         if r.status_code != 200:
@@ -55,50 +52,41 @@ class ModDBDownloader(DefaultDownloader):
 
         return g_session.get(f"https://www.moddb.com{s[0]}", allow_redirects=False).headers["location"]
 
-    def check(self, dl_dir: Path, update_cache: bool = False) -> None:  # noqa: C901
+    def _set_vars_from_metadata(self):
+        if not self._iurl:
+            return
+
+        try:
+            metadata = self._parse_moddb_metadata(self._iurl) if self._iurl else {}
+
+            self._archivehash = metadata.get('MD5 Hash', None)
+            self._user_wanted_name = metadata.get('Filename', None)
+        except HTTPError:
+            metadata = {}
+
+        return metadata
+
+    def check(self, to: Path, update_cache: bool = False) -> None:
         if not self._iurl:
             raise HashError('No Info URL provided for this mod')
 
-        try:
-            info = self._parse_moddb_data(self._iurl)
-            file = dl_dir / info['Filename']
-            hash = info['MD5 Hash']
-        except ConnectionError as e:
-            raise HashError(f'Error while fetching moddb page for {self._iurl}\n  -> {e}')
-        except KeyError:
-            raise HashError(f'Error while parsing moddb page for {self._iurl}')
-        except HTTPError as e:
-            raise HashError(f'ModDB parsing error for {self._iurl}\n -> {e}')
+        metadata = self._set_vars_from_metadata()
 
-        if info.get('Download', '') not in self._url:
-            raise HashError(f'Skipping {file.name} since ModDB info do not match download url')
+        if not self._user_wanted_name:
+            raise ModDBDownloadError(f'Could not find Filename in {self._iurl}')
+
+        if not self._archivehash:
+            raise ModDBDownloadError(f'Could not find archive hash in {self._iurl}')
+
+        if metadata.get('Download', '') not in self._url:
+            raise ModDBDownloadError(f'Skipping {self._user_wanted_name} since ModDB info do not match download url')
 
         self._url = self._get_download_url(self._url)
-        self._archive = file
 
-        if not self._archive.exists():
-            if update_cache:
-                super().download(dl_dir)
-            return
-
-        if check_hash(self._archive, hash):
-            return
-
-        if not update_cache:
-            raise HashError(f'{file.name} MD5 missmatch')
-
-        self._archive.unlink()
-        super().download(dl_dir)
-        super().download(dl_dir, use_cached=True, hash=hash)  # to check hash
+        super().check(to, update_cache)
 
     def download(self, to: Path, use_cached: bool = False, *args, **kwargs) -> Path:
-        try:
-            metadata = self._parse_moddb_data(self._iurl) if self._iurl else {}
-        except Exception:
-            metadata = {}
-
-        hash = metadata.get('MD5 Hash', None) if metadata.get('Download', '') in self._url else None
-
+        self._set_vars_from_metadata()
         self._url = self._get_download_url(self._url)
-        self._archive = to / os.path.basename(urlparse(self._url).path)
-        return super().download(to, use_cached, hash=hash)
+
+        return super().download(to, use_cached)
